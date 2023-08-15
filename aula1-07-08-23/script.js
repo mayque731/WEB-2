e2fe4ea7ce23d8de312be191308c75cdf1365100
Mayque <mayque.731@gmail.com>
2023-08-14 21:40:00 -0400

    aula 14-08

--- aula1-07-08-23/script.js
+++ aula1-07-08-23/script.js
@@ -1,7 +1,7 @@
 //let, var - valor pode ser alterado
 //const - valor nao poder ser alterado
 
-var nome = "Mayque Oliveira";
+var name = "Mayque Oliveira";
 let numero = 69999448231;
 const PI = 3.14;
 let logico = true;
@@ -36,8 +36,51 @@ console.log("media igual a ", media);
 //console.log(PI);
 
 //tipagem fraca
-if (PI === "3.14") {
+if (PI === 3.14) {
   console.log("Tudo normal");
 } else {
   console.log("Algo de errado nao esta certo");
 }
+
+function soma() {
+  n1 = 1;
+  n2 = 2;
+  let n3 = n1 + n2;
+  return n3;
+  console.log(n3);
+}
+//soma();
+
+let quartos = 4;
+let m2 = 3000;
+
+function preco(m, q) {
+  if (q === 1) {
+    return m * 1;
+  } else if (q === 2) {
+    return m * 1.2;
+  } else if (q === 3) {
+    return m * 1.5;
+  } else {
+    return "impossivel definir valor";
+  }
+}
+
+console.log(preco(m2, quartos));
+
+// arrays
+
+let classes = ["warlock", "mage", "sorcerer", "monk", "warrior", "assassin"];
+
+console.log(classes[0]);
+
+let carros = ["BMW", "Ferrari", "Mercedes"];
+
+//1
+console.log(carros[1]);
+
+carros[1] = "Audi";
+
+carros.push("Volvo");
+
+console.log(carros.length);
