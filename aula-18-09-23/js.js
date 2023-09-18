5cb452f2bd5c3500ad83c3d1c6c20b5ebb516f3c
Mayque <mayque.731@gmail.com>
2023-09-18 19:52:16 -0400

    corrigida separação em blocos e nomes de variaveis

--- aula-18-09-23/js.js
+++ aula-18-09-23/js.js
@@ -2,14 +2,14 @@ async function post() {
   let postagem = document.querySelector("#post");
   let response = await fetch("https://jsonplaceholder.typicode.com/posts");
   let json = await response.json();
-}
 
-if (json.length > 0) {
-  postagem.innerHTML = "";
-  for (let i in json) {
-    let postHTML =
-      "<div><h1>" + json[i].title + "</h1>" + json[i].body + "</div>";
-    postagem.innerHTML = postagem.innerHTML + postsHTML;
+  if (json.length > 0) {
+    postagem.innerHTML = "";
+    for (let i in json) {
+      let postsHTML =
+        "<div><h1>" + json[i].title + "</h1>" + json[i].body + "</div>";
+      postagem.innerHTML = postagem.innerHTML + postsHTML;
+    }
   }
 }
 post();
