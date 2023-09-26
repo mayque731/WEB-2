2ccfca4cc3e7a6b33ed387529056e1647cd0ba66
Mayque <mayque.731@gmail.com>
2023-09-25 21:47:25 -0400

    puchando os nomes da API

--- consumo-api-mayque/js.js
+++ consumo-api-mayque/js.js
@@ -1,3 +1,24 @@
-function apertou() {
-  console.log("apertou");
+async function post() {
+  let postagem = document.querySelector("#resul");
+  let response = await fetch("https://rickandmortyapi.com/api/character/");
+  let json = await response.json();
+
+  console.log(json.results[5].id);
+
+  if (json.results.length > 0) {
+    postagem.innerHTML = "";
+    for (let i in json.results) {
+      let postsHTML = "<div id='bloco-cont'>" + json.results[i].name + "</div>";
+      postagem.innerHTML = postagem.innerHTML + postsHTML;
+    }
+  } else {
+    console.error;
+    alert("Algo deu errado!:/");
+    postagem.innerHTML = "ERRO!";
+  }
+}
+post();
+
+function search() {
+  console.log("pesquisou");
 }
