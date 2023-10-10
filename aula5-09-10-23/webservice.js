670f576b47d0d8131de921a2601cff86462c7d14
Mayque <mayque.731@gmail.com>
2023-10-09 21:40:14 -0400

    dependencias instaladas, teste de consumo api iniciado

---
+++ aula5-09-10-23/webservice.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("characters", async (req, res) => {
+  try {
+    const res = await axios.get("https://swapi.dev/api/people");
+    res.json(res.data);
+  } catch (error) {
+    res.status(500).send("errp ao buscar");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server up on port 3000");
+});
