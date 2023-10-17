55b4687042c2b327a2f5a438dbc14324c39451af
Mayque <mayque.731@gmail.com>
2023-10-16 20:22:47 -0400

    conclusao da aula 5, consumo api star wars

--- aula5-09-10-23/consumo.js
+++
@@ -1,1 +0,0 @@
-//consumir da api
---
+++ aula5-09-10-23/public/consumo.js
@@ -0,0 +1,37 @@
+async function post() {
+  let postagem = document.querySelector("#lista_personagens");
+  let response = await fetch("https://swapi.dev/api/people/");
+  let json = await response.json();
+
+  console.log(json.results);
+
+  if (json.results.length > 0) {
+    //postagem.innerHTML = "";
+    for (let i in json.results) {
+      let postsHTML =
+        "<tr><td>" +
+        json.results[i].name +
+        "</td><td>" +
+        json.results[i].height +
+        "</td><td>" +
+        json.results[i].mass +
+        "</td><td>" +
+        json.results[i].hair_color +
+        "</td><td>" +
+        json.results[i].skin_color +
+        "</td><td>" +
+        json.results[i].eye_color +
+        "</td><td>" +
+        json.results[i].birth_year +
+        "</td><td>" +
+        json.results[i].gender +
+        "</td></tr>";
+      postagem.innerHTML = postagem.innerHTML + postsHTML;
+    }
+  } else {
+    console.error;
+    alert("Algo deu errado!:/");
+    postagem.innerHTML = "ERRO!";
+  }
+}
+post();
